a5357d91957508f75ff9fa33384effdf0f18bd16
Vlad M <vladimir.martynenko.work@gmail.com>
2016-03-17 15:33:58 +0200

    comments for the mime types and defibs tasks

--- Easy/Defibrillators.js
+++ Easy/Defibrillators.js
@@ -8,19 +8,22 @@ var LAT = parseFloat(readline().replace(',', '.'));
 var N = parseInt(readline());
 
 var closestResult = '';
-var closestDistance = 99999999999999;
+const closestDistance = 99999999999999;
 
 for (var i = 0; i < N; i++) {
     var DEFIB = readline();
 
     var parsedDefibData = DEFIB.split(';');
-
+    
+    // defib longtitude
     var longB = parseFloat(parsedDefibData[4].replace(',', '.'));
+    // defib latitude
     var latB = parseFloat(parsedDefibData[5].replace(',', '.'));
-
+    
     var x = (longB - LON) * Math.cos((LAT + latB) / 2);
     var y = latB - LAT;
-
+    
+    // distance between defib and a person
     var distance = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2)) * 6371;
 
     if (closestDistance > distance) {
--- Easy/MIME_Type.js
+++ Easy/MIME_Type.js
@@ -6,24 +6,30 @@
 var N = parseInt(readline());
 var Q = parseInt(readline());
 
-var mymeTypes = new Map();
+var mimeTypes = new Map();
 
+// get association between MIME types and extensions
 for (var i = 0; i < N; i++) {
     var inputs = readline().split(' ');
     var EXT = inputs[0];
     var MT = inputs[1];
 
-    mymeTypes.set(EXT.toLowerCase(), MT);
+    mimeTypes.set(EXT.toLowerCase(), MT);
 }
 
+// display correct results for each extension passed
 for (var i = 0; i < Q; i++) {
     var FNAME = readline();
     var res = FNAME.split('.');
 
+    // this checks that there is at least string for name of the file and name for extension after dot
+    // this check covers following items : 1) a 2) .pdf - both should be UNKNOWN
     if (res.length > 1) {
         var extension = res[res.length - 1].toLowerCase();
-        if (mymeTypes.has(extension)) {
-            print(mymeTypes.get(extension));
+        
+        //
+        if (mimeTypes.has(extension)) {
+            print(mimeTypes.get(extension));
         }
         else {
             print('UNKNOWN');
