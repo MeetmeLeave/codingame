a8ba40c50abc82249bb3495f6e069e6a8f2ba43c
Vlad M <vladimir.martynenko.work@gmail.com>
2016-03-29 19:22:49 +0300

    minify, uglify

--- Code Golf/Temperatures.js
+++ Code Golf/Temperatures.js
@@ -1,41 +1,28 @@
-/**
- * Auto-generated code below aims at helping you parse
- * the standard input according to the problem statement.
- **/
+var n = parseInt(readline());
 
-var n = parseInt(readline()); // the number of temperatures to analyse
-
-// if there are no numbers return 0
-// if values are present, parse them out of the string and find value closest to zero
 if (n > 0) {
-    var temps = readline(); // the n temperatures expressed as integers ranging from -273 to 5526
-    var tempArray = temps.split(' ');
-    var result = tempArray[0];
-    var resultAbs = Math.abs(result);
+    var t = readline();
+    var a = t.split(' ');
+    var r = a[0];
+    var b = Math.abs(r);
 
     for (var i = 1; i < n; i++) {
-        // get next temperature value and its abs
-        var currentValue = tempArray[i];
-        var currentAbs = Math.abs(currentValue);
+        var g = a[i];
+        var c = Math.abs(g);
         
-        // set new closest value if its abs is closer to zero  
-        if (currentAbs < resultAbs) {
-            result = currentValue;
-            resultAbs = currentAbs;
+        if (c < b) {
+            r = g;
+            b = c;
         }
-        // set new closest value if it is positive variant of the same number
-        else if (currentAbs === resultAbs) {
-            if (currentValue > result) {
-                result = currentValue;
-                resultAbs = currentAbs;    
+        else if (c === b) {
+            if (g > r) {
+                r = g;
+                b = c;    
             }
         }
     }
 
-    // Write an action using print()
-    // To debug: printErr('Debug messages...');
-
-    print(result);
+    print(r);
 }
 else {
     print(0);
