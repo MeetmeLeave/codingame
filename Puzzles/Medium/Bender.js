e226a65345dec4bc618381ba35bae491e0faccb9
Vlad Martynenko <VMartynenko@lattice-engines.com>
2016-06-13 11:28:52 +0300

    Finished Bender solution

--- Puzzles/Medium/Bender.js
+++ Puzzles/Medium/Bender.js
@@ -1,5 +1,7 @@
 'use strict';
 
+// Constants
+const STATE_EMPTY = 'EMPTY';
 const STATE_START = 'START';
 const STATE_DEAD = 'DEAD';
 const STATE_ERROR = 'ERROR';
@@ -23,6 +25,10 @@ const TILE_BEAR = 'B';
 const TILE_TELEPORT = 'T';
 const TILE_EMPTY = ' ';
 
+// Magic value ^_^
+const TURNS_BEFORE_LOOP = 100;
+
+// Map
 let map = [];
 
 let t1X = -1;
@@ -30,6 +36,15 @@ let t1Y = -1;
 let t2X = -1;
 let t2Y = -1;
 
+function MapNode(x, y, value) {
+    this.x = x;
+    this.y = y;
+    this.value = value;
+    this.isVisited = false;
+    this.direction = ''
+}
+
+// Bender ( ‾ʖ̫‾)
 function Bender() {
     this.currentState = STATE_START;
     this.isInBreakerMode = false;
@@ -37,57 +52,86 @@ function Bender() {
     this.currentTile = TILE_START;
     this.currentX = -1;
     this.currentY = -1;
+    this.loopCounter = 0;
+    this.path = [];
 }
 
-Bender.prototype.isDead = function () {
+Bender.prototype.isDead = function() {
     return this.currentState === STATE_DEAD;
 };
 
-Bender.prototype.isInLoop = function () {
+Bender.prototype.isInLoop = function() {
     return this.currentState === STATE_LOOP;
 };
 
-Bender.prototype.setPosition = function (x, y) {
+Bender.prototype.setPosition = function(x, y) {
     this.currentX = x;
     this.currentY = y;
 };
 
-Bender.prototype.updatePosition = function () {
+Bender.prototype.updatePosition = function() {
     switch (this.currentState) {
         case STATE_SOUTH:
-            this.currentY += 1;
-            break;
-        case STATE_EAST:
             this.currentX += 1;
             break;
+        case STATE_EAST:
+            this.currentY += 1;
+            break;
         case STATE_NORTH:
-            this.currentY -= 1;
+            this.currentX -= 1;
             break;
         case STATE_WEST:
-            this.currentX -= 1;
+            this.currentY -= 1;
             break;
         default:
             return;
     }
 
-    this.currentTile = map[this.currentX][this.currentY];
+    let tile = map[this.currentX][this.currentY];
+    if (!tile.isVisited) {
+        tile.isVisited = true;
+        tile.direction = this.currentState;
+        this.loopCounter -= 1;
+    }
+
+    if (tile.isVisited && tile.direction == this.currentState) {
+        this.loopCounter += 1;
+    }
+
+    this.currentTile = tile.value;
 };
 
-Bender.prototype.move = function () {
-    printErr('previousState: ' + this.currentState + ' !@#$%');
+Bender.prototype.buildPath = function(params) {
+    // Update this part for move set prediction
+    while (!bender.isDead() && !bender.isInLoop()) {
+        bender.move();
+    }
+
+    return this.path;
+};
+
+Bender.prototype.move = function() {
+    // printErr('previousState: ' + this.currentState + ' !@#$%');
     this.updatePosition();
     this.updateState();
+    // printErr('coordinates: X: ' + this.currentX + '; Y: ' + this.currentY + ' !@#$%');
     printErr('currentState: ' + this.currentState + ' !@#$%');
-    printErr('###########');
-    return this.currentState;
+    // printErr('###########');
 };
 
-Bender.prototype.updateState = function () {
+Bender.prototype.updateState = function() {
     let nextState = this.currentState;
-    printErr('currentTile: ' + this.currentTile + ' !@#$%');
+
+    if (this.loopCounter >= TURNS_BEFORE_LOOP) {
+        this.path = [];
+        this.currentState = STATE_LOOP;
+        this.path.push(this.currentState);
+        return;
+    }
+
+    // printErr('currentTile: ' + this.currentTile + ' !@#$%');
     switch (this.currentTile) {
         case TILE_EMPTY:
-            printErr('EMPTY');
             break;
         case TILE_START:
             if (this.currentState == STATE_START) {
@@ -95,7 +139,7 @@ Bender.prototype.updateState = function () {
             }
             break;
         case TILE_OBSTACLE:
-            map[this.currentX][this.currentY] = TILE_EMPTY;
+            map[this.currentX][this.currentY].value = TILE_EMPTY;
             break;
         case TILE_SOUTH:
             nextState = STATE_SOUTH;
@@ -118,8 +162,7 @@ Bender.prototype.updateState = function () {
         case TILE_TELEPORT:
             if (this.currentX === t1X && this.currentY === t1Y) {
                 this.setPosition(t2X, t2Y);
-            }
-            else {
+            } else {
                 this.setPosition(t1X, t1Y);
             }
             break;
@@ -127,96 +170,104 @@ Bender.prototype.updateState = function () {
             nextState = STATE_DEAD;
             break;
         default:
-            printErr('ERROR');
             nextState = STATE_ERROR;
             break;
     }
 
     if (nextState != STATE_DEAD && nextState != STATE_ERROR) {
-        nextState = this.getNextMovableState(nextState);
+        let moves = this.getNextMovableState(nextState);
+
+        // DEBUG
+        // for (let i = 0; i < moves.length - 1; i++) {
+        //     this.path.push(moves[i]);
+        // }
+
+        nextState = moves[moves.length - 1];
+    } else {
+        this.currentState = nextState;
+        return;
     }
 
     this.currentState = nextState;
+    this.path.push(this.currentState);
 };
 
-Bender.prototype.getNextMovableState = function (direction) {
-    let nextMoveState = 'ERROR';
+Bender.prototype.getNextMovableState = function(direction) {
+    let nextMoveStates = [];
 
     let tile = this.getMapTileByDirection(direction);
-
     if (!this.canMoveOnTile(tile)) {
         let priorities = this.getPossibleMovePriorities();
 
         for (let i = 0; i < priorities.length; i++) {
             let priority = priorities[i];
-            printErr('priority: ' + priority);
             tile = this.getMapTileByDirection(priority);
-
+            nextMoveStates.push(priority);
             if (this.canMoveOnTile(tile)) {
-                nextMoveState = priority;
-                printErr('selected priority: ' + priority);
                 break;
             }
         }
-    }
-    else {
-        nextMoveState = direction;
+    } else {
+        nextMoveStates.push(direction);
     }
 
-    return nextMoveState;
+    return nextMoveStates;
 };
 
-Bender.prototype.canMoveOnTile = function (tile) {
-    if (tile == TILE_BORDER || tile == TILE_OBSTACLE && !this.isInBreakerMode) {
-        return false;
+Bender.prototype.canMoveOnTile = function(tile) {
+    let canMove = true;
+
+    if (tile == TILE_BORDER) {
+        canMove = false;
+    } else if (tile == TILE_OBSTACLE && !this.isInBreakerMode) {
+        canMove = false;
     }
 
-    return true;
+    return canMove;
 };
 
-Bender.prototype.getMapTileByDirection = function (direction) {
+Bender.prototype.getMapTileByDirection = function(direction) {
     let x = this.currentX;
-    let y = this.currentX;
-
+    let y = this.currentY;
     switch (direction) {
         case STATE_SOUTH:
-            y += 1;
-            break;
-        case STATE_EAST:
             x += 1;
             break;
+        case STATE_EAST:
+            y += 1;
+            break;
         case STATE_NORTH:
-            y -= 1;
+            x -= 1;
             break;
         case STATE_WEST:
-            x -= 1;
+            y -= 1;
             break;
         default:
             break;
     }
 
-    return map[x][y];
+    return map[x][y].value;
 };
 
-Bender.prototype.getPossibleMovePriorities = function () {
+Bender.prototype.getPossibleMovePriorities = function() {
     let possibleMoves = [];
 
     if (this.isMovePriorityReversed) {
-        possibleMoves.push(TILE_WEST);
-        possibleMoves.push(TILE_NORTH);
-        possibleMoves.push(TILE_EAST);
-        possibleMoves.push(TILE_SOUTH);
-    }
-    else {
-        possibleMoves.push(TILE_SOUTH);
-        possibleMoves.push(TILE_EAST);
-        possibleMoves.push(TILE_NORTH);
-        possibleMoves.push(TILE_WEST);
+        possibleMoves.push(STATE_WEST);
+        possibleMoves.push(STATE_NORTH);
+        possibleMoves.push(STATE_EAST);
+        possibleMoves.push(STATE_SOUTH);
+    } else {
+        possibleMoves.push(STATE_SOUTH);
+        possibleMoves.push(STATE_EAST);
+        possibleMoves.push(STATE_NORTH);
+        possibleMoves.push(STATE_WEST);
     }
 
     return possibleMoves;
 };
 
+// Play the FSM
 let bender = new Bender();
 
 let inputs = readline().split(' ');
@@ -228,7 +279,7 @@ for (let i = 0; i < L; i++) {
     printErr(row);
     for (let j = 0; j < row.length; j++) {
         let tileVal = row[j];
-        map[i][j] = tileVal;
+        map[i][j] = new MapNode(i, j, tileVal);
 
         if (tileVal == TILE_START) {
             bender.setPosition(i, j);
@@ -238,8 +289,7 @@ for (let i = 0; i < L; i++) {
             if (t1X === -1) {
                 t1X = i;
                 t1Y = j;
-            }
-            else {
+            } else {
                 t2X = i;
                 t2Y = j;
             }
@@ -249,12 +299,9 @@ for (let i = 0; i < L; i++) {
 
 printErr('---------');
 
-// Update this part for move set prediction
-if (bender.isInLoop()) {
-    print(STATE_LOOP);
-}
+let path = bender.buildPath();
+printErr('path: ' + path);
+for (let i = 0; i < path.length; i++) {
 
-while (!bender.isDead() || !bender.isInLoop()) {
-    let benderMove = bender.move();
-    print(benderMove);
+    print(path[i]);
 }
