5b64269116e352f504b7ab53c787820c5ec2f949
Vlad Martynenko <VMartynenko@lattice-engines.com>
2016-07-02 02:26:17 +0300

    busters temp

--- Contest/CodeBusters.js
+++ Contest/CodeBusters.js
@@ -1,39 +1,92 @@
-/**
- * Send your busters out into the fog to trap ghosts and bring them home!
- **/
+'use strict';
+
+const STATE_SPAWNED = 'SPAWNED';
+const STATE_RETREAT = 'RETREAT';
+const STATE_SEAK = 'SEAK';
+const STATE_RELEASE = 'RELEASE';
+const STATE_CAPTURE = 'CAPTURE';
+
+class Mobile {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.id = undefined;
+    }
+
+    get currentX() {
+        return this.x;
+    }
+
+    set currentX(value) {
+        this.x = value;
+    }
+
+    get ID() {
+        return this.id;
+    }
+
+    set ID(value) {
+        if (this.id === undefined) {
+            this.id = value;
+        }
+    }
+}
+
+class Buster extends Mobile {
+    constructor(x, y, teamId) {
+        super(x, y);
+        this.team = teamId;
+        this.state = STATE_SPAWNED;
+        this.losRange = 2200;
+        this.speed = 800;
+        this.hasGhost = false;
+        this.currentTargetMobile = undefined;
+    }
+}
+
+class Ghost extends Mobile {
+    constructor(x, y) {
+        super(x, y);
+        this.speed = 400;
+        this.isCaptured = false;
+        this.isReleased = false;
+    }
+}
+
+class Planner {
+    constructor(teamId, ghostCount, bustersPerPlayer) {
+        this.potentialGhostsCount = ghostCount;
+        this.teamId = teamId;
+        this.ghostCount = ghostCount;
+        this.bustersCount = bustersPerPlayer;
+
+        this.busters = new Map();
+        this.ghosts = new Map();
+        this.enemyBusters = new Map();
+
+        if (teamId === 0) {
+            this.basePositionX = 0;
+            this.basePositionY = 0;
+            this.enemyBasePositionX = 16000;
+            this.enemyBasePositionY = 9000;
+        } else {
+            this.basePositionX = 16000;
+            this.basePositionY = 9000;
+            this.enemyBasePositionX = 0;
+            this.enemyBasePositionY = 0;
+        }
+    }
+
+    updateMobile(id, x, y, type, state, value) {
+
+    }
+}
 
 var bustersPerPlayer = parseInt(readline()); // the amount of busters you control
 var ghostCount = parseInt(readline()); // the amount of ghosts on the map
 var myTeamId = parseInt(readline()); // if this is 0, your base is on the top left of the map, if it is one, on the bottom right
 
-function Buster(x, y) {
-    this.currentX = x;
-    this.currentY = y;
-    this.currentState = 'SPAWNED';
-}
-
-function Planner(teamId, ghostCount, bustersPerPlayer) {
-    this.potentialGhostsCount = ghostCount;
-    this.teamId = teamId;
-    this.busters = [];
-    this.ghosts = [];
-    this.enemyBusters = [];
-
-    if (teamId === 0) {
-        this.basePositionX = 0;
-        this.basePositionY = 0;
-    } else {
-        this.basePositionX = 16000;
-        this.basePositionY = 9000;
-    }
-
-    for (let i = 0; i < bustersPerPlayer; i++) {
-        let buster = new Buster(this.basePositionX, this.basePositionY);
-        this.busters.push(buster);
-    }
-}
-
-let base = new Planner();
+let planner = new Planner();
 
 // game loop
 while (true) {
